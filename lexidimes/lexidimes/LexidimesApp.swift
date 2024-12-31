f274afddefe600eea84fca34ebcbbc262b14eab2
smaarmstrong <108431423+smaarmstrong@users.noreply.github.com>
2024-12-31 12:38:30 +0000

    start project with gitiginore

---
+++ lexidimes/lexidimes/LexidimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  lexidimesApp.swift
+//  lexidimes
+//
+//  Created by Sean Armstrong on 12/31/24.
+//
+
+import SwiftUI
+
+@main
+struct lexidimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
