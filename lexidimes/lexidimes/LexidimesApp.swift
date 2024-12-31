11a692f558bb82595b9161886e16f7088ae2ddcd
smaarmstrong <108431423+smaarmstrong@users.noreply.github.com>
2024-12-31 12:39:28 +0000

    linting + configs

--- lexidimes/lexidimes/LexidimesApp.swift
+++ lexidimes/lexidimes/LexidimesApp.swift
@@ -1,17 +1,10 @@
-//
-//  lexidimesApp.swift
-//  lexidimes
-//
-//  Created by Sean Armstrong on 12/31/24.
-//
-
 import SwiftUI
 
 @main
-struct lexidimesApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct LexidimesApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
